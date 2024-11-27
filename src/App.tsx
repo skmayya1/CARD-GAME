833f23c6b979cc2548fe064de5c8a1b75634ebf9
rohan <hastag2404@gmail.com>
2024-11-27 19:21:24 +0530

    create room page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 
 const App = () => {
   return (
-    <div className="">App</div>
+    <div className="bg-[#191818] h-screen w-full flex items-center justify-center">
+      <button className="bg-[#33312E] font-mono text-center px-6 py-3 w-[40vh] text-3xl text-white rounded-lg ">Create Room</button>
+
+    </div>
   )
 }
 
