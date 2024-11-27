e7e03e1484d74bfead13da38969a891cdd075fa7
rohan <hastag2404@gmail.com>
2024-11-27 19:59:43 +0530

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import CreateRoom from "./Components/CreateRoom"
+import Room from "./Components/Room";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <CreateRoom />
+  },
+  {
+    path : "/room",
+    element:<Room />
   }
 ])
 const App = () => {
