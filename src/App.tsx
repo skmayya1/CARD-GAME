9e7d0b199e8f65d8a118f51b4e875807277ba933
rohan <hastag2404@gmail.com>
2024-11-27 19:34:45 +0530

    added headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 
-const App = () => {
+
+
+const App: React.FC = () => {
+ 
+
   return (
-    <div className="bg-[#191818] h-screen w-full flex items-center justify-center">
-      <button className="bg-[#33312E] font-mono text-center px-6 py-3 w-[40vh] text-3xl text-white rounded-lg ">Create Room</button>
+    <div className="bg-[#191818] h-screen w-full">
+      
+      <div className="flex justify-between items-center p-4 bg-gray-800 shadow-md">
+        <h1 className="text-2xl text-white font-bold">Andar Bahar</h1>
+       
+      </div>
 
+      
+      <div className="h-full flex items-center justify-center">
+        <button
+          className="bg-[#33312E] font-mono text-center px-6 py-3 w-[40vh] text-3xl text-white rounded-lg"
+          
+        >
+          Create Room
+        </button>
+      </div>
     </div>
-  )
-}
 
-export default App
+   
+    
+  );
+};
+
+export default App;
