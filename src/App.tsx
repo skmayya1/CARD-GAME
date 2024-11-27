ba10f098b007d4573c69cddaf33535bb5f13f888
skmayya1 <skandamayya4@gmail.com>
2024-11-27 20:00:40 +0530

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import CreateRoom from "./Components/CreateRoom"
+import "./Utils/Socket"
 
 const router = createBrowserRouter([
   {
