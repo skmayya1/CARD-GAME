18529c4c0c6021476909f58da12239fc2007d498
skmayya1 <skandamayya4@gmail.com>
2024-11-27 19:44:10 +0530

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import CreateRoom from "./Components/CreateRoom"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CreateRoom />
+  }
+])
 const App = () => {
-  return (
-    <div className="bg-[#191818] h-screen w-full flex items-center justify-center">
-      <button className="bg-[#33312E] font-mono text-center px-6 py-3 w-[40vh] text-3xl text-white rounded-lg ">Create Room</button>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
